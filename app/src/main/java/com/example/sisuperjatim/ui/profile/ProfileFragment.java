c7d1609943a4e963d39b3ce81a45e6a96faa9a74
anggi lds <anggilds01@gmail.com>
2020-01-15 13:24:37 +0700

    Project BPTP (SiSuper Jatim)

--- app/src/main/java/com/example/sisuperjatim/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/sisuperjatim/ui/profile/ProfileFragment.java
@@ -1,32 +1,32 @@
 package com.example.sisuperjatim.ui.profile;
 
-import android.content.Intent;
+//import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+//import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
+//import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
-import com.example.sisuperjatim.ProfileActivity;
+//import com.example.sisuperjatim.ProfileActivity;
 import com.example.sisuperjatim.R;
-import java.time.Instant;
+//import java.time.Instant;
 
 public class ProfileFragment extends Fragment {
     private ProfileViewModel profileViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        profileViewModel =
-                ViewModelProviders.of(this).get(ProfileViewModel.class);
+        profileViewModel = ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
 
 
         return root;
     }
+
 }
 

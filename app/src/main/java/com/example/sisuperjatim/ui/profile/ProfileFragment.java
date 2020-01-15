88a360fa70a94577870d83224016233b914d3ac1
anggi lds <anggilds01@gmail.com>
2020-01-16 02:00:32 +0700

    Project BPTP (SiSuper Jatim)

--- app/src/main/java/com/example/sisuperjatim/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/sisuperjatim/ui/profile/ProfileFragment.java
@@ -1,7 +1,9 @@
 package com.example.sisuperjatim.ui.profile;
 
 //import android.content.Intent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,22 +11,39 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 //import androidx.cardview.widget.CardView;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 //import com.example.sisuperjatim.ProfileActivity;
+import com.example.sisuperjatim.ProfileActivity;
 import com.example.sisuperjatim.R;
 //import java.time.Instant;
 
 public class ProfileFragment extends Fragment {
+    CardView cardView;
     private ProfileViewModel profileViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        profileViewModel = ViewModelProviders.of(this).get(ProfileViewModel.class);
+        profileViewModel =
+                ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-
-
+//        final TextView textView = root.findViewById(R.id.text_notifications);
+//        profileViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        cardView = root.findViewById(R.id.input_data);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("TestInput", "onClick: ");
+                startActivity(new Intent(getActivity(), ProfileActivity.class));
+            }
+        });
         return root;
     }
 

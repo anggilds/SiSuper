9020bf2c1b37d6f8c6c071cd6540e757fc80de28
Bram <q.vier23@gmail.com>
2020-02-11 08:02:42 +0700

    LoggedView need design improve

--- app/src/main/java/com/example/sisuperjatim/LoginActivity.java
+++ app/src/main/java/com/example/sisuperjatim/LoginActivity.java
@@ -32,7 +32,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText username,password;
     private Button login;
     private TextView link_register;
-    private static String URL_LOGIN = "http://192.168.11.111/bptp/login.php";
+    private static String URL_LOGIN = "http://192.168.1.15/bptp/login.php";
 
 
     @Override
@@ -79,18 +79,26 @@ public class LoginActivity extends AppCompatActivity {
                         try {
                             JSONObject jsonObject = new JSONObject(response);
                             String success = jsonObject.getString("success");
+                            JSONArray jsonArray = jsonObject.getJSONArray("login");
+
                             if (success.equals("1")){
+                                for (int i = 0; i < jsonArray.length(); i ++ ){
+                                    JSONObject object = jsonArray.getJSONObject(i);
+                                    String name = object.getString("name").trim();
+                                    String email = object.getString("email").trim();
 
                                     Toast.makeText(LoginActivity.this, "Logged in! \n"
                                             , Toast.LENGTH_SHORT).show();
-//                                    Intent intent = new Intent(LoginActivity.this, ProfileFragment.class);
-//                                    intent.putExtra("name", name);
-//                                    intent.putExtra("email", email);
-//                                    startActivity(intent);
-
-
+                                    Intent intent = new Intent(LoginActivity.this, ProfileActivityLogged.class);
+                                    intent.putExtra("name", name);
+                                    intent.putExtra("email", email);
+                                    startActivity(intent);
                                 }
 
+                            }else if(success.equals("0")){
+                                Toast.makeText(LoginActivity.this, "Wrong username or password! \n"
+                                        , Toast.LENGTH_SHORT).show();
+                            }
                         } catch (JSONException e){
                             e.printStackTrace();
                             Toast.makeText(LoginActivity.this, "Something's wrong\nDetails : "+
--- app/src/main/java/com/example/sisuperjatim/ProfileActivityLogged.java
+++ app/src/main/java/com/example/sisuperjatim/ProfileActivityLogged.java
@@ -2,23 +2,34 @@ package com.example.sisuperjatim;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProfileActivityLogged extends AppCompatActivity {
+SessionManager sessionManager;
+private TextView name, email;
+private Button logout;
 
-    private EditText login, input_data;
 
-    ImageView ivBackProfile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_profile_logged);
 
+        sessionManager = new SessionManager(this);
+        sessionManager.checkLogin();
 
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        logout = findViewById(R.id.logout);
 
 
     }
